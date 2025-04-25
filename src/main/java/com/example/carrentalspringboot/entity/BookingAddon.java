8f24ea4a5b2d5bf0b4908c32be05438063d9418b
Tigran Karapetyan <tigrankarapetyana@gmail.com>
2025-04-25 08:48:31 +0400

    💚 ci (composite key) update types

--- src/main/java/com/example/carrentalspringboot/entity/BookingAddon.java
+++ src/main/java/com/example/carrentalspringboot/entity/BookingAddon.java
@@ -7,44 +7,17 @@ import jakarta.persistence.*;
 @Table(name = "booking_addon")
 public class BookingAddon {
     @EmbeddedId
-    private BookingAddonId id;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @MapsId("bookingId")
-    @JoinColumn(name = "booking_id", nullable = false)
-    private Booking booking;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @MapsId("addonId")
-    @JoinColumn(name = "addon_id", nullable = false)
-    private Addon addon;
+    private BookingAddonId bookingAddonId;
 
     public BookingAddon() {
     }
 
-    public BookingAddon(Booking booking, Addon addon) {
-        this.booking = booking;
-        this.addon = addon;
+    public BookingAddon(BookingAddonId id) {
+        this.bookingAddonId = id;
     }
 
-    public BookingAddonId getId() {
-        return id;
-    }
-
-    public Booking getBooking() {
-        return booking;
-    }
-
-    public void setBooking(Booking booking) {
-        this.booking = booking;
-    }
-
-    public Addon getAddon() {
-        return addon;
-    }
-
-    public void setAddon(Addon addon) {
-        this.addon = addon;
+    public BookingAddonId getBookingAddonId() {
+        return bookingAddonId;
     }
 }
 
