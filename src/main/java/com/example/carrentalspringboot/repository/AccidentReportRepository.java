f288855dbb813221b171872a042da2536db46cf0
Tigran Karapetyan <tigrankarapetyana@gmail.com>
2025-04-25 10:41:13 +0400

    💚 ci () create JPA repository

--- src/main/java/com/example/carrentalspringboot/repository/AccidentReportRepository.java
+++ src/main/java/com/example/carrentalspringboot/repository/AccidentReportRepository.java
@@ -3,5 +3,6 @@ package com.example.carrentalspringboot.repository;
 import com.example.carrentalspringboot.entity.AccidentReport;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AcidentReportRepository extends JpaRepository<AccidentReport, Integer> {
+public interface AccidentReportRepository extends JpaRepository<AccidentReport, Integer> {
+    boolean existsAccidentReportByBookingId(Integer id);
 }
